d92615da9eb20df9880980ea00d5d4ab99a4bf15
Mehmet Fatih Okuyan <okuyanf6161@gmail.com>
2018-01-03 22:09:38 +0300

    can scan files now.
    Users can use files for scanning.

--- ipfinder.py
+++ ipfinder.py
@@ -1,27 +1,61 @@
 import urllib.request
 import re
 
-# importing txt address
+# deciding file or url
 
-data = urllib.request.urlopen("https://isc.sans.edu/block.txt").readlines()
+chooser = str(input("URL of File?: (U/F)  "))
 
-# changing data type as string
+if chooser == "U":
 
-rexData = str(data)
+    # getting url from user. Ex: https://isc.sans.edu/block.txt
 
-# defining pattern
+    inputURL = str(input("Input URL: "))
 
-ip = re.findall("[0-9]{0,3}\.[0-9]{0,3}\.[0-9]{0,3}\.[0-9]{0,3}", rexData)
+    # getting data from url with urllib
 
-# opening a txt file for writing the output
+    inputFile = str(urllib.request.urlopen(inputURL).readlines())
 
-text_file = open("Output.txt", "w")
+    # opening a file for writing the output
 
-# writing the output
+    outputFile = str(input("Output File Name: "))
+    text_file = open(outputFile, "w")
 
-for i in ip:
-    text_file.write(i + "\n")
+    # defining pattern
 
-text_file.close()
+    ip = re.findall("[0-9]{0,3}\.[0-9]{0,3}\.[0-9]{0,3}\.[0-9]{0,3}", inputFile)
 
-# Mehmet Fatih Okuyan
+    # writing the output to the file
+
+    for i in ip:
+        text_file.write(i + "\n")
+
+    text_file.close()
+
+elif chooser == "F":
+
+    # opening the file that contains data. Ex: log.txt
+
+    inputFile = str(input("Input File Name: "))
+    infile = open(inputFile, "r")
+
+    # opening a file for writing the output
+
+    outputFile = str(input("Output File Name: "))
+    text_file = open(outputFile, "w")
+
+    for line in infile:
+        line = line.strip()
+
+        # defining pattern
+
+        ip = re.findall("[0-9]{0,3}\.[0-9]{0,3}\.[0-9]{0,3}\.[0-9]{0,3}", line)
+
+        # writing the output to the file
+
+        for i in ip:
+            text_file.write(i + "\n")
+
+    text_file.close()
+
+else:
+    print("Wrong Input")
